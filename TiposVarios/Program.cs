6d90e0e692cce67448cfe154b5f72163c1d41f1b
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-02 21:41:04 +0000

    Exercicio 3 imprimir dois chars

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -8,12 +8,21 @@ namespace TiposVarios
     {
         static void Main()
         {
+            //exercicio 3 
+            //integer variables exercice
             int intnumber=1;
             uint uintnumber=2;
             byte bytenumber=3;
             Console.WriteLine(intnumber);
             Console.WriteLine(uintnumber);
             Console.WriteLine(bytenumber);
+
+            //char variables exercice
+            char char1='\u3246';
+            char char2='\u3247';
+            Console.WriteLine(char1);
+            Console.WriteLine(char2);
+
         }
     }
 }
