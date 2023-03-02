738c7f88ae49c18537e75083221d82b902df8207
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-02 22:04:50 +0000

    Exercicio 4, strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //exercicio 4
+            //string variables
+            string string1="qwerty";
+            string string2="@";
+            string string3="asdfg";
+            string string4="@";
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
+            Console.WriteLine(string3);
+            Console.WriteLine(string4);
+
+        }
+    }
+}
