a8c82b955b95ca47e1a34878e3b2d4fca2c92584
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-02 21:23:47 +0000

    Exercicio 3 variaveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main()
+        {
+            int intnumber=1;
+            uint uintnumber=2;
+            byte bytenumber=3;
+            Console.WriteLine(intnumber);
+            Console.WriteLine(uintnumber);
+            Console.WriteLine(bytenumber);
+        }
+    }
+}
