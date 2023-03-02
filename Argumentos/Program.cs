27e2b70be54caf3d9f8ee86132993522dca66f9d
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-02 20:13:34 +0000

    Exercicio 1

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //Exercicio 1
+           //writes the arguments recieve one by one
+            for (int i = 0; i < args.Length; i++) 
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
