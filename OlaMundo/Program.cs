07b55163221727e0abc97140a29e91d7d29490be
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-03 11:04:14 +0000

    Exercicio 2,XML

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// class program
+    /// </summary>
     class Program
-    {
+    {  
+        /// <summary>
+        /// Main void
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
