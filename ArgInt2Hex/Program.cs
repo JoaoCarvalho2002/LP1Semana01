43ec848b2f48a2c0a42d77a245fdb3fce24de53d
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-03 14:32:11 +0000

    Exercicio 7,receber inteiro e transformar em hexadecimal

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArgInt2Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"for" to run through the Array args
+            for (int i=0;i<=args.Length;i++){
+            //for each element the "If" will verify if there is a number or not
+            //if there is a number, it will print it and convert it to Hex   
+                if(int.TryParse(args[i],out int n1)){
+                    Console.WriteLine("Inteiro: "+n1);
+                    Console.WriteLine("Numero em Hexadecimal: "+n1.ToString("X"));
+                }
+            //if its not a number it will say so    
+                else {
+                    Console.WriteLine("Não é um numero!");
+
+                }
+            }
+        }
+    }
+}
