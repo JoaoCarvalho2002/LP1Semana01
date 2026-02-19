7cdf57f53580b1f61efdf153c6f93c5defbb6fdb
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-02-19 16:02:50 +0000

    Add EO1

---
+++ EO1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EO1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
