3cfb7535b823bb18a53b819f4ef3107ee4ce6d70
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-02-19 16:23:32 +0000

    Changed project name

---
+++ Extra01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extra01
+{
+    //U+00AE ->registered trademark
+    // exercicio: imprimir o codigo
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            //char trademark= ’\U+00AE’
+            Console.WriteLine("U+00AE");
+            //Console.WriteLine(trademark);
+        }
+    }
+}
